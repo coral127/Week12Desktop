427cdef4bdeaa3b2b63821102827c32d93c041b7
coral127 <sasahappy90@gmail.com>
2025-07-12 21:21:02 +0900

    Week13
    16. 스테이터스 마무리

--- Week12Desktop/Assets/scripts/PlayerController.cs
+++ Week12Desktop/Assets/scripts/PlayerController.cs
@@ -137,7 +137,7 @@ public class PlayerController : MonoBehaviour
     //점프 시도
     private void TryJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isGround && theStatusController.GetCurrentSP() > 0)
         {
             Jump();
         }
@@ -155,12 +155,12 @@ public class PlayerController : MonoBehaviour
     //달리기 시도
     private void TryRun()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) && isGround && theStatusController.GetCurrentSP() > 0)
         {
             Runing();
         }
         
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.LeftShift) || theStatusController.GetCurrentSP() <=0)
         {
             RuningCancel();
         }
