dd3a3346b82d3f621be8db1770710661b2474100
coral127 <sasahappy90@gmail.com>
2025-07-13 21:25:35 +0900

    Week14
    24. 시야각 구현

--- Week12Desktop/Assets/scripts/NPC/Pig.cs
+++ Week12Desktop/Assets/scripts/NPC/Pig.cs
@@ -128,7 +128,7 @@ public class Pig : MonoBehaviour
         anim.SetBool("Walking", isWalking);
         Debug.Log("°È±â");
     }
-    private void Run(Vector3 _targetPos)
+    public void Run(Vector3 _targetPos)
     {
         direction = Quaternion.LookRotation(transform.position -  _targetPos).eulerAngles;
 
